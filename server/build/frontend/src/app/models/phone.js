079d8a70055a5a7b40a78485f5a1ceb2b6466519
Adrià Navarrete Pardo <adri.navarrete.09@gmail.com>
2019-11-06 23:23:42 +0100

    primera parte frontend

--- server/build/frontend/src/app/models/phone.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Phone {
-    constructor(key = '', value = '') {
-        this.key = key;
-        this.value = value;
-    }
-}
-exports.Phone = Phone;
--- server/build/frontend/src/app/models/phone.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const phone_1 = require("./phone");
-describe('Phone', () => {
-    it('should create an instance', () => {
-        expect(new phone_1.Phone()).toBeTruthy();
-    });
-});
